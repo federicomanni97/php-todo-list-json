d4070a8a28571c42c507e2d9d14348873318309b
federicomanni97 <manni_federico@libero.it>
2023-12-01 16:53:41 +0100

    if server.php for push in todolist[ ] the object

--- server.php
+++ server.php
@@ -3,6 +3,15 @@ $filetodo = file_get_contents("todo-list.json");
 
 $todoList = json_decode($filetodo, true);
 
+if (isset($_POST['task'])){
+    $newObject = [
+        "text" => $_POST['task'],
+        "done" => false 
+    ];
+    array_push($todoList, $newObject);
+    file_put_contents('todo-list.json', json_encode($todoList));
+}
+
 header('Content-Type: application/json');
 echo json_encode($todoList);
 ?>
